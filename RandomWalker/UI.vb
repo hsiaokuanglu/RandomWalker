1f71610a50d1f1c46e474d5f8dece7f692dee968
Ray L <rraylu403@gmail.com>
2019-07-19 14:16:43 +0800

    Clean up some dead code

--- RandomWalker/UI.vb
+++ RandomWalker/UI.vb
@@ -8,34 +8,6 @@ Public Class UI
     Private world As World
     Private walker As Walker
 
-    Private Sub UI_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
-    Private Sub Start()
-
-        'Dim fileLoc = $"C:\test\RW\animationfile\{width}x{length}_{types}.txt"
-        Dim fileLoc As String = Me.dataLoc_textBox.Text
-
-        Dim newWorld = New World(_width, _length, _bin)
-        Dim walker = New Walker(0, 0, 1, newWorld)
-
-        'walker.RandomFill()
-        'walker.OrderFill()
-        walker.MapFillnAnimate()
-        newWorld.PrintReserve()
-        'newWorld.PrintCoordinate()
-        WriteFile(newWorld, walker, fileLoc)
-        Dim renderer = New Renderer(_width, _length, _bin, fileLoc)
-        'renderer.RenderImage($"C:\test\RW\animationfile\{width}x{length}_{types}_map.jpg")
-        renderer.RenderImage(Me.outputLoc_text.Text)
-        Console.WriteLine("Done Rendering")
-        ''Walker Path Recorder
-        newWorld.WPR.WriteToFile(TextBox1.Text)
-        Console.WriteLine($"Jump Number: {walker.JumpNum}")
-        Console.WriteLine($"Efficiency = {newWorld.WPR.CalDistance() / (_width * _length - 1)} times more")
-        'Console.ReadKey()
-    End Sub
-
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         _width = Me.tbx.Text
         _length = Me.tby.Text
